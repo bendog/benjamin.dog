fb4989bd1a90ced1ba15127e6b0623a1fbcb4d63
Ben Fitzhardinge <ben.fitzhardinge@ausvet.com.au>
2019-05-07 15:00:50 +0800

    remove .DS_Store from deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws s3 sync ~/dev/benjamin.dog/ s3://benjamin.dog/ --exclude="*.git/*" --exclude="_docs/*"
+aws s3 sync ~/dev/benjamin.dog/ s3://benjamin.dog/ --exclude="*.git/*" --exclude="_docs/*" --exclude=".DS_Store" --exclude="*/.DS_Store"
